7ef143dda243ec2e6a55df08bfcbc6c868948fa5
AlexKsenz <sany00ksenz@gmail.com>
2022-08-12 19:15:53 +0300

    Logout

---
+++ input/input.php
@@ -0,0 +1,19 @@
+<div class="zeroblock clearfix"></div>
+<section id="content">
+    <form action="#" method="POST">
+        <h1>Вхід</h1>
+        <div>
+            <input type="email" placeholder="Ваш email" required="" id="username" name="email" />
+        </div>
+        <div>
+            <input type="password" placeholder="Пароль" required="" id="password" name="password" />
+        </div>
+
+
+
+        <div>
+            <input type="submit" value="Увійти" />
+            
+            <a href="register.php">Зареєструватися</a>
+        </div>
+    </form><!-- form --><div class="zeroblock clearfix"></div>
