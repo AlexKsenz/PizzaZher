2573395c76391825eb1010f6ff4cb11bf95dafb0
AlexKsenz <sany00ksenz@gmail.com>
2022-08-12 23:57:41 +0300

    BD

--- pizzazher.sql
+++ pizzazher.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Авг 12 2022 г., 21:55
+-- Время создания: Авг 12 2022 г., 23:57
 -- Версия сервера: 5.7.33
 -- Версия PHP: 7.1.33
 
@@ -24,6 +24,60 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `catalog`
+--
+
+CREATE TABLE `catalog` (
+  `id` int(11) NOT NULL,
+  `id_composition` int(11) NOT NULL,
+  `id_category` int(11) NOT NULL,
+  `price` float NOT NULL,
+  `weight` int(11) NOT NULL,
+  `img` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `category_name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `composition`
+--
+
+CREATE TABLE `composition` (
+  `id` int(11) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `compound` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `final`
+--
+
+CREATE TABLE `final` (
+  `id` int(11) NOT NULL,
+  `id_users` int(255) NOT NULL,
+  `id_composition` varchar(255) NOT NULL,
+  `order_sum` int(11) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `time_now` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `users`
 --
 
@@ -36,19 +90,48 @@ CREATE TABLE `users` (
   `role` varchar(255) NOT NULL DEFAULT 'uzer'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
 --
--- Дамп данных таблицы `users`
+-- Структура таблицы `users_cart`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `phone`, `role`) VALUES
-(1, '', 'admin@mail.ua', '123', 'фффф', 'uzer'),
-(2, 'ййй', 'admin@mail.ua', 'ййй', 'ййй', 'uzer'),
-(3, 'admin', 'admin@mail.ua', '123', '0632235132', 'uzer'),
-(4, 'си', 'admin@mail.ua', 'си', 'си', 'uzer');
+CREATE TABLE `users_cart` (
+  `id` int(11) NOT NULL,
+  `id_composition` int(11) NOT NULL,
+  `id_users` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Индексы сохранённых таблиц
 --
+
+--
+-- Индексы таблицы `catalog`
+--
+ALTER TABLE `catalog`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_category` (`id_category`),
+  ADD KEY `id_composition` (`id_composition`);
+
+--
+-- Индексы таблицы `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `composition`
+--
+ALTER TABLE `composition`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `final`
+--
+ALTER TABLE `final`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_users` (`id_users`);
 
 --
 -- Индексы таблицы `users`
@@ -57,14 +140,76 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Индексы таблицы `users_cart`
+--
+ALTER TABLE `users_cart`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_composition` (`id_composition`),
+  ADD KEY `id_users` (`id_users`);
+
+--
 -- AUTO_INCREMENT для сохранённых таблиц
 --
+
+--
+-- AUTO_INCREMENT для таблицы `catalog`
+--
+ALTER TABLE `catalog`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `composition`
+--
+ALTER TABLE `composition`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `final`
+--
+ALTER TABLE `final`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `users_cart`
+--
+ALTER TABLE `users_cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `catalog`
+--
+ALTER TABLE `catalog`
+  ADD CONSTRAINT `catalog_ibfk_1` FOREIGN KEY (`id_category`) REFERENCES `category` (`id`),
+  ADD CONSTRAINT `catalog_ibfk_2` FOREIGN KEY (`id_composition`) REFERENCES `composition` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `final`
+--
+ALTER TABLE `final`
+  ADD CONSTRAINT `final_ibfk_1` FOREIGN KEY (`id_users`) REFERENCES `users` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `users_cart`
+--
+ALTER TABLE `users_cart`
+  ADD CONSTRAINT `users_cart_ibfk_1` FOREIGN KEY (`id_composition`) REFERENCES `composition` (`id`),
+  ADD CONSTRAINT `users_cart_ibfk_2` FOREIGN KEY (`id_users`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
